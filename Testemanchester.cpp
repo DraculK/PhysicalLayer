224c784ecc3e55375bf4b91a4e69672f84703ae5
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2021-09-13 17:15:11 -0300

    Refatorando codManchester

--- Testemanchester.cpp
+++ Testemanchester.cpp
@@ -1,68 +1,63 @@
 /* Este arquivo é apenas um modelo, vai ser apagado posteriormente*/
 #include <iostream>
+#include <vector>  
+using namespace std;  
+int i, j = 0;
+/* i é utilizado para bit e j para manchester */
 
-int manchester() {
-	int bit[8] = {0, 1, 0, 1, 0, 0, 1, 1};
-	int manchester[16];
-	int clock = 1;
-	int i, j = 0;
-	/* i é utilizado para bit e j para manchester */
-	
-	/* printar */
-	for(i = 0; i < 8; i++){
-		std::cout << bit[i];
+void cod_manchester(vector<int> bit, vector<int>& manchester, int clock){
+	if(clock == 0 && bit[i] == 0){
+		manchester.push_back(0);
+	}else if(clock == 0 && bit[i] == 1){
+		manchester.push_back(1);
+	}else if(clock == 1 && bit[i] == 0){
+		manchester.push_back(1);
+	}else if(clock == 1 && bit[i] == 1){
+		manchester.push_back(0);
 	}
-	std::cout << "\n" << std::endl;
+}
+
+void dec_manchester(vector<int>& novo_bit, vector<int> manchester){
+	int clock = 0;
+	for(j = 0; j < manchester.size(); j++){
+		if((clock % 2) == 1){ /* simulando um clock */
+			if(manchester[j] == 0){
+				novo_bit.push_back(1);
+			}else if(manchester[j] == 1){
+				novo_bit.push_back(0);
+			}
+		}
+		clock++;
+	}
+}
+
+void printar(vector<int> bit){
+	for(i = 0; i <bit.size(); i++){
+		cout << bit[i];
+	}
+	cout << "\n" << endl;
+}
+
+int main() {
+	vector<int> bit;
+	vector<int> novo_bit;
+	vector<int> manchester;
+	bit.insert(bit.end(), {0, 1, 0, 1, 0, 0, 1, 1});
+	int clock = 1;
+	printar(bit);
 
 	/* Codificação Manchester */
-	for(i = 0; i < 8; i++){
-		clock = 0;
-		if(clock == 0 && bit[i] == 0){
-			manchester[j] = 0;
-		}else if(clock == 0 && bit[i] == 1){
-			manchester[j] = 1;
-		}else if(clock == 1 && bit[i] == 0){
-			manchester[j] = 1;
-		}else if(clock == 1 && bit[i] == 1){
-			manchester[j] = 0;
-		}
-		j += 1;
-		clock = 1;
-		if(clock == 0 && bit[i] == 0){
-			manchester[j] = 0;
-		}else if(clock == 0 && bit[i] == 1){
-			manchester[j] = 1;
-		}else if(clock == 1 && bit[i] == 0){
-			manchester[j] = 1;
-		}else if(clock == 1 && bit[i] == 1){
-			manchester[j] = 0;
-		}
-		j += 1;
+	for(i = 0; i < bit.size(); i++){
+		clock = 0;	/* simulando um clock */
+		cod_manchester(bit, manchester, clock);
+		clock = 1;	/* simulando um clock */
+		cod_manchester(bit, manchester, clock);
 	}
-	
-	/* printar */
-	for(i = 0; i < (8*2); i++){
-		std::cout << manchester[i];
-	}
-	std::cout << "\n" << std::endl;
+	printar(manchester);
 
 	/* Decodificação manchester */
-	for(j = 1; j < (8*2); j+=2){
-		clock = 1;
-		/* (clock % 2 == 0) */
-		/* (clock == 0) ? (faz nada) : (executa o if) */
-		if(manchester[j] == 0){
-			bit[i] = 1;
-		}else if(manchester[j] == 1){
-			bit[i] = 0;
-		}
-	}
-	
-	/* printar */
-	for(i = 0; i < 8; i++){
-		std::cout << bit[i];
-	}
-	std::cout << "\n" << std::endl;
+	dec_manchester(novo_bit, manchester);
+	printar(novo_bit);
 	
     return 0;
 }

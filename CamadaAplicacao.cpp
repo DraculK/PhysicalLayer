d7bab851eb40dc35e88cdb10c1ca74aca2e4a546
Draculk <kayranvieira456@hotmail.com>
2021-09-13 19:22:51 -0300

    :memo: documentaçao

--- CamadaAplicacao.cpp
+++ CamadaAplicacao.cpp
@@ -18,14 +18,17 @@ vector<int> StringToBit(string mensagem){
   vector<int> quadro;
   string mensagemBit;
   
+  //para fazer a conversão, utilizamos bitset para conseguir transformar nossa mensagem em um binário no formato de string.
   for(int i = 0; i<mensagem.size(); i++) {
     mensagemBit += std::bitset<8>(mensagem[i]).to_string();
   }
 
+  //Aqui mostramos o resultado da conversão.
   int tamanho = mensagemBit.length();
   cout << "A mensagem em binário é: ";
   cout << mensagemBit << endl;
 
+  //Nesse for, estamos comparando cada item da String com seu valor ASCII. Sendo 0 = 48 e 1 = 49. De acordo com o valor, colocamos 0 ou 1 no vetor quadro.
   for(int i = 0; i < tamanho; i++){
     if (mensagemBit[i] == 48){
       quadro.push_back(0);
@@ -46,20 +49,25 @@ vector<int> StringToBit(string mensagem){
 }
 
 string BitToString(vector<int> quadro){
-  string mensagem, final;
+  string mensagemBinario, final;
   vector<bitset<8>> conversor;
 
+  //Nesse for, estamos passando por todos os itens do quadro e transformando em string para facilitar o trabalho depois.
   for(int i = 0; i < quadro.size(); i++){
-    mensagem += to_string(quadro[i]);
+    mensagemBinario += to_string(quadro[i]);
     
   }
-    stringstream sstream(mensagem);
-    while (sstream.good())
-    {
-        bitset<8> bits;
-        sstream >> bits;
-        final += char(bits.to_ulong());
-    }
+
+  //Nessa parte, começamos a utilizar a biblioteca sstream, pois essa biblioteca facilita na hora da conversão.
+
+  //Aqui, usamos as bibliotecas sstream e bitset para converter a variável 'mensagemBinario' em bits e armazenamos os bits na variável 'paraBits'. Após isso, fazemos a variável final receber 'char(paraBits.to_ulong())', pois o 'to_ulong' retorna o valor com a mesma representação do bitset, no nosso caso, usamos o char, pois queremos letras e não interos. Com isso, temos a conversão pronta.
+  stringstream sstream(mensagemBinario);
+  while (sstream.good())
+  {
+    bitset<8> paraBits;
+    sstream >> paraBits;
+    final += char(paraBits.to_ulong());
+  }
   cout << "O texto decodificado é: ";
   cout << final << endl;
   return final;

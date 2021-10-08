3b379a0c13f8a1627933b878b1a7d3dfaeedba17
Draculk <kayranvieira456@hotmail.com>
2021-10-08 19:00:49 -0300

    :sparkles: header do enlace

---
+++ CamadaEnlace.h
@@ -0,0 +1,27 @@
+#include "CamadaFisica.h" 
+
+//TRANSMISSÃO
+vector<int> CamadaEnlaceDadosTransmissora(vector<int>);
+
+vector<int> CamadaEnlaceDadosTransmissoraEnquadramento (vector<int>);
+
+vector<int> CamadaEnlaceDadosTransmissoraControleDeErro(vector<int>);
+
+vector<int> CamadaDeEnlanceTransmissoraEnquadramento(vector<int>);
+
+vector<int> CamadaDeEnlanceTransmissoraControleDeErro(vector<int>);
+
+vector<int> CamadaEnlaceDadosTransmissoraEnquadramentoContagemDeCaracteres(vector<int>);
+
+vector<int> CamadaEnlaceDadosTransmissoraEnquadramentoInsercaoDeBytes(vector<int>);
+
+//RECEPÇÃO
+vector<int> CamadaEnlaceDadosReceptora(vector<int>);
+
+vector<int> CamadaEnlaceDadosReceptoraEnquadramento(vector<int>);
+
+vector<int> CamadaDeEnlaceReceptoraControleDeErro(vector<int>);
+
+vector<int> CamadaEnlaceDadosReceptoraEnquadramentoContagemDeCaracteres(vector<int>);
+
+vector<int> CamadaEnlaceDadosReceptoraEnquadramentoInsercaoDeBytes(vector<int>);
